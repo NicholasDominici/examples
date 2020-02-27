e4cfb93213feb62229f5a09c1a46081739b9e93b
Nicholas Dominici <imnick43@gmail.com>
2020-02-26 22:49:31 -0500

    Added argument, --modelname

--- fast_neural_style/Train.bat
+++ fast_neural_style/Train.bat
@@ -1,4 +1,4 @@
 cd C:\AI Tools\fast_neural_style
-python neural_style/neural_style.py train --dataset images/faketraining --style-image images/style-images/simp.jpg --save-model-dir trainedmodel --checkpoint-model-dir _checkpoint --checkpoint-interval 1000 --content-weight 1e8 --style-weight 1e11 --epochs 2 --cuda 1 --batch-size 8
+python neural_style/neural_style.py train --dataset images/training-images --style-image images/style-images/starry.jpg --save-model-dir trainedmodel --modelname starry --content-weight 1e5 --style-weight 1e10 --epochs 1 --cuda 1 --batch-size 6
 
 pause
